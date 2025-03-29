b6cf9f3201f8d8a44167666795824b4ca8f2e686
Krushovice <kickstar69@yandex.ru>
2025-03-29 16:26:14 +1000

    add case_converter to db config

--- bot/core/config.py
+++ bot/core/config.py
@@ -1,9 +1,30 @@
+import logging
 from pathlib import Path
+from typing import Literal
 
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+LOG_DEFAULT_FORMAT = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
+
+
+class LoggingConfig(BaseModel):
+    log_level: Literal[
+        "debug",
+        "info",
+        "warning",
+        "error",
+        "critical",
+    ] = "info"
+    log_format: str = LOG_DEFAULT_FORMAT
+
+    @property
+    def log_level_value(self) -> int:
+        return logging.getLevelNamesMapping()[self.log_level.upper()]
 
 
 class DbConfig(BaseModel):
@@ -12,6 +33,14 @@ class DbConfig(BaseModel):
     echo_pool: bool = False
     pool_size: int = 5
     max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class BotConfig(BaseModel):
@@ -26,6 +55,7 @@ class Settings(BaseSettings):
     db: DbConfig
     bot: BotConfig
     admin: AdminConfig
+    logging: LoggingConfig = LoggingConfig()
 
     model_config = SettingsConfigDict(
         env_file="./.env",
